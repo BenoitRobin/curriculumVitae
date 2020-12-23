0882f79de56bae0abae0505fc5c0c457001f2932
BenoitRobin <36993553+BenoitRobin@users.noreply.github.com>
2020-12-23 08:42:25 +0100

    Initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<style type="text/scss">
+
+</style>
+
+<nav>
+	<ul>
+		<li><a href=".">home</a></li>
+		<li><a href="about">about</a></li>
+	</ul>
+</nav>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+</script>
+
+<style type='text/scss'>
+@import "../styles/theme.scss";
+
+	main {
+		position: relative;
+		max-width: $site-width;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav />
+
+<main >
+	<slot></slot>
+</main>
